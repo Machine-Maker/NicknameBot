17a424832daa7423f74c46b15470231ae46b4914
Jake Potrebic <jake.m.potrebic@gmail.com>
2019-07-21 21:27:51 -0700

    add reset command

---
+++ commands/util/reset.js
@@ -0,0 +1,22 @@
+const Commando = require('discord.js-commando')
+
+module.exports = class Reset extends Commando.Command {
+  constructor(client) {
+    super(client, {
+      name: 'reset',
+      group: 'admin',
+      memberName: 'reset',
+      description: 'Resets the configuration',
+      details: 'Resets both channel configurations for the guild',
+      examples: ['reset'],
+      guildOnly: true
+    })
+  }
+
+  run(msg) {
+    // TODO: Add confirmation
+    this.client.provider.remove(msg.guild, 'channel.request')
+    this.client.provider.remove(msg.guild, 'channel.receive')
+    msg.reply('Reset the configuration')
+  }
+}
